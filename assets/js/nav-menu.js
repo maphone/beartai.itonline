a8e06e7831900905f944a288104ffb47f8a556c3
maphone.thai1995@hotmail.com <maphone.thai1995@hotmail.com>
2023-12-07 11:03:32 +0700

    New Update Beartai-Itonline

---
+++ assets/js/nav-menu.js
@@ -0,0 +1,29 @@
+// const btnMenu = document.getElementById("btn-nav");
+// const manuBar = document.getElementById("menu_Bar");
+
+
+
+function openNav() {
+    document.getElementById("menu_Bar").style.left = "0px";
+}
+
+
+function closeNav() {
+    document.getElementById("menu_Bar").style.left = "-380px";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
